d8467eceb2133eefe6cf31d1459b5b0e2453997a
Om Shukla <omshukla8573@gmail.com>
2026-04-17 01:02:49 +0530

    Initial commit-Marketing Analytics Project

---
+++ sql/dim_customers.sql
@@ -0,0 +1,15 @@
+-- SQL statement to join dim_customers with dim_geography to enrich customer data with geographic information
+
+SELECT 
+    c.CustomerID,
+    c.CustomerName,
+    c.Email,
+    c.Gender,
+    c.Age,
+    g.Country,
+    g.City  
+FROM 
+    dbo.customers as c  
+LEFT JOIN dbo.geography g
+ON 
+    c.GeographyID = g.GeographyID;  -- Joins the two tables on the GeographyID field to match customers with their geographic information
